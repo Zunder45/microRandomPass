9fdac6aa53ffb4d07ae852e93b1ac49b2c991c74
Zunder45 <sashaorlov02y@yandex.ru>
2021-08-09 00:55:57 +0500

    добвлен -l, исправлено количество вывода символов

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import random as r
 import argparse
 
 length = 5 
+itr = 1
 
 en_alph = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L" ,"M", "N", "O", "P", "Q", "R", "S", "T", "V", "X", "Y", "Z" ]
 char = ["`","~","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","}","[","]","\\","\|",":",";","\"","\'","<",">",",",".","?","/"]
+
+
 
 def rand_word():
     res = ""
@@ -14,12 +17,46 @@ def rand_word():
     else:
         return en_alph[r.randint(0,len(en_alph)-1)]
     
-
 def rand_num():
-    return r.randint(0,10)
+    return r.randint(0,9)
 
 def rend_char():
-    return str(char[r.randint(0,len(char))])
+    return str(char[r.randint(0,len(char)-1)])
+
+
+
+
+def rand_func_select():
+    return r.randint(1,len(rand_func))
+
+def rand():
+    res = ""
+    i = 0
+    while i < int(length):
+        res += str(rand_func[rand_func_select()]())
+        i += 1
+    print(res)
+
+
+
+
+def pars(): 
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-l",help="length", type=int)
+    parser.add_argument("-i",type=int)
+
+    arg = parser.parse_args()
+
+    if arg.i !=  None:
+        global itr
+        itr = arg.i
+
+    if arg.l != None:
+        global length
+        length = arg.l
+    
+
 
 rand_func = {
     1: rand_num,
@@ -28,36 +65,13 @@ rand_func = {
 }
 
 
-def rand_func_select():
-    return r.randint(1,len(rand_func))
-def rand():
-    res = ""
-    i = 0
-    while i <= int(length):
-        res += str(rand_func[rand_func_select()]())
-        i += 1
-    print(res)
-
-
-def pars():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument("-l",help="length", type=int)
-
-
-    arg = parser.parse_args()
-
-    if arg.l != None:
-        global length
-        length = arg.l
-    
-
-
-
-
 def main():
-    pars()
-    rand()
+    i = 0
+    while i < itr: 
+        pars()
+        rand()
+    
+        i += 1
 
 
 main()
