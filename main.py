837e03e47dc855dcf0885ab60fc3ff424f921969
Zunder45 <sashaorlov02y@yandex.ru>
2023-02-05 18:54:17 +0500

    rewritten from scratch

--- main.py
+++ main.py
@@ -1,48 +1,111 @@
-import random as r
-import argparse
-import rand, var
+"""
+-----------
+    1.0
+-----------
+    -h - Помощь
+    -l - Длина пароля
+    -i - Количество паролей
+    -n - Цифры
+    -a - Маленькие буквы
+    -A - Большие букыв
+    -c - Симвалы
+"""
 
 
-def pars(): 
-    parser = argparse.ArgumentParser()
 
-    parser.add_argument("-l","--length",help="length", type=int)
-    parser.add_argument("-i","--iteration",type=int)
-    parser.add_argument("-n","--num",action="store_const", const="True")
-    parser.add_argument("-c","--char",action="store_const", const="True")
-    parser.add_argument("-a","--alph",action="store_const", const="True")
+import conf
+import sys, random
 
-    arg = parser.parse_args()
+def getValArg():
+    args = sys.argv
 
-    if arg.iteration !=  None:
-        var.itr = arg.iteration
+    valsArgs = {
+        "len": 8,
+        "itr": 1,
+        "num": False,
+        "chr": False,
+        "aup": False,
+        "adw": False,
+    }
+    if "-h" in args:
+        printHelp()
+        exit()
 
-    if arg.length != None:
-        var.length = arg.length
-    if not any((arg.num,arg.char,arg.alph)):
-        var.allChar()
-    else:
-        if arg.num != None:
-            var.onlyNum = True
-        if arg.char != None:
-            var.onlyChar = True
-        if arg.alph != None:
-            var.onlySymAlph = True
+    if len(args) == 1:
+        valsArgs = {
+            "len": 8,
+            "itr": 1,
+            "num": True,
+            "chr": True,
+            "aup": True,
+            "adw": True,
+        }
+        return valsArgs
 
-    # print(var.onlyNum, var.onlyChar, var.onlyStr)
+    if "-l" in args:
+        if args.index("-l") + 1 < len(args):
+            valsArgs["len"] = args[args.index("-l") + 1]
+    if "-i" in args:
+        if args.index("-i") + 1 < len(args):
+            valsArgs["itr"] = args[args.index("-i") + 1]
+    if "-n" in args:
+        valsArgs["num"] = True
+    if "-c" in args:
+        valsArgs["chr"] = True
+    if "-a" in args:
+        valsArgs["adw"] = True
+    if "-A" in args:
+        valsArgs["aup"] = True
 
+    return valsArgs
+
+def printHelp():
+    print(__doc__)
+
+def getPasswd(args):
     
+    passwdListChar = []
+
+    listArgForGen = []
+
+    if args["num"] == True:
+        listArgForGen.append("num")
+    if args["chr"] == True:
+        listArgForGen.append("chr")
+    if args["adw"] == True:
+        listArgForGen.append("adw")
+    if args["aup"] == True:
+        listArgForGen.append("aup")
     
+    for i in range(int(args['itr'])):
+        strPass = ""
+        for j in range(int(args['len'])):
+            start = random.choice(listArgForGen)
+
+            if start == "num":
+                strPass += str(random.randrange(0,10))
+            elif start == "adw":
+                strPass += str(random.choice(conf.lett)).lower()
+            elif start == "aup":
+                strPass += random.choice(conf.lett).upper()    
+            elif start == "chr":
+                strPass += random.choice(conf.char)
+            
+        passwdListChar.append(strPass)
+
+    return passwdListChar
+
 
 def main():
-    pars()
-    i = 0
-    while i < var.itr: 
-        rand.rand_main()
-        i += 1
+    passList = getPasswd(getValArg())
+
+    for p in passList:
+        print(p)
 
 
-main()
+if __name__ == "__main__":
+    main()
+
 
 
 
